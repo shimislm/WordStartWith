9d1f9fd9c3acda652dd274578c6ce535f9baa0ff
shimislm <shimislm5@gmail.com>
2022-08-10 20:19:56 +0300

    add images

--- lettersClass.js
+++ lettersClass.js
@@ -7,12 +7,33 @@ class Letters{
     }
     render(){
         document.querySelector(this.parent).innerHTML = `
-        <h2 class="bg-dark text-white col-1 display-3">${this.letter}</h2>
+        <h2 class="bg-dark text-white col-1">${this.letter}</h2>
         <div id="id_words" class="mt-5 row row-cols-md-3 row-cols-1 align-items-center justify-content-center gap-4">
-            <button id="answer1" class="btn btn-info col-12">${this.answers[0]}</button>
-            <button id="answer2" class="btn btn-info col-12">${this.answers[1]}</button>
-            <button id="answer3" class="btn btn-info col-12">${this.answers[2]}</button>
-            <button id="answer4" class="btn btn-info col-12">${this.answers[3]}</button>
+        <div id="answer1" class="card" style="width: 150px; cursor: pointer;">
+        <img src="${this.answers[0].image}" class="card-img-top" width="100%" alt="${this.answers[0].name}">
+        <div class="card-body">
+          <h5 class="card-title">${this.answers[0].name}</h5>
+        </div>
+      </div>
+        <div id="answer2" class="card" style="width: 150px; cursor: pointer;">
+        <img src="${this.answers[1].image}" class="card-img-top" width="100%" alt="${this.answers[1].name}">
+        <div class="card-body">
+          <h5 class="card-title">${this.answers[1].name}</h5>
+        </div>
+      </div>
+        <div id="answer3" class="card" style="width: 150px; cursor: pointer;">
+        <img src="${this.answers[2].image}" class="card-img-top" width="100%" alt="${this.answers[2].name}">
+        <div class="card-body">
+          <h5 class="card-title">${this.answers[2].name}</h5>
+        </div>
+      </div>
+        <div id="answer4" class="card" style="width: 150px; cursor: pointer;">
+        <img src="${this.answers[3].image}" class="card-img-top" width="100%" alt="${this.answers[3].name}">
+        <div class="card-body">
+          <h5 class="card-title">${this.answers[3].name}</h5>
+        </div>
+      </div>
+
         </div>
         `
         let flag = false;
@@ -23,13 +44,13 @@ class Letters{
         let btn3 = document.querySelector("#answer3")
         let btn4 = document.querySelector("#answer4")
         let btns_ar = [btn1, btn2, btn3, btn4];
-        btns_ar.forEach((e) => {
+        btns_ar.forEach((e, index) => {
             e.addEventListener("click", () => {
                 if (!flag) {
                     flag = true;
                     // the rest in red
                     e.style.background = "red";
-                    if (e.innerHTML.charAt(0) === this.letter) {
+                    if (e.querySelector(".card-title").innerHTML[0] === this.letter) {
                         e.style.background = "green";
                         document.body.style.background = "lightgreen";
                         setTimeout(function () {
@@ -40,7 +61,7 @@ class Letters{
                         document.body.style.background = "rgb(255, 104, 104)";
                         // if answer == correctAsnwer than the background change green else red
                         btns_ar.forEach((e) => {
-                            e.style.background = e.innerHTML.charAt(0) == this.letter ? "green" : "red";
+                            e.style.background = e.querySelector(".card-title").innerHTML[0] == this.letter? "green" : "red";
                         })
                         // e.style.background = "red";
                         setTimeout(function () {
--- startAt.js
+++ startAt.js
@@ -1,12 +1,13 @@
 let counter = -1;
 let words = [], letters = [];
 let lettersArr = ["א","ב","ג","ד","ה","ו","ז","ח","ט","י","כ","ל","מ","נ","ס","ע","פ","צ","ק","ר","ש","ת"]
-let wordArr = ["אריה","בית","אבטיח","ביצים","גמל","גיטרה","דבורה","דב","הר","הליקופטר","ורד","וילון","זבוב","זברה","חתול","חולצה","טלוויזיה","טבח","יתוש","ינשוף","כלב","כוכב","ליצן","לטאה","מספריים","מחשב","נחש","נדנדה","סירה","סוס","פרה","פיג'מה","ציור","צב","קנגורו","קוף","רמזור","רופא","שולחן","שעון","תמונה","תותים","ענף","צדף","עכבר","עגבניה","סבון","ענן","נעלים","לוייתן","לחם","עציץ"]
+// let wordArr = ["אריה","בית","אבטיח","ביצים","גמל","גיטרה","דבורה","דב","הר","הליקופטר","ורד","וילון","זבוב","זברה","חתול","חולצה","טלוויזיה","טבח","יתוש","ינשוף","כלב","כוכב","ליצן","לטאה","מספריים","מחשב","נחש","נדנדה","סירה","סוס","פרה","פיג'מה","ציור","צב","קנגורו","קוף","רמזור","רופא","שולחן","שעון","תמונה","תותים","ענף","צדף","עכבר","עגבניה","סבון","ענן","נעלים","לוייתן","לחם","עציץ"]
 const init = ()=>{
     letters = _.shuffle(lettersArr);
     words = _.shuffle(wordArr);
     createTrivia();
 }
+
 const createTrivia = ()=>{
     counter++;
     if(counter>= letters.length){
@@ -15,13 +16,13 @@ const createTrivia = ()=>{
     }
     let answers =[]
     let rnd = Math.floor(Math.random()*words.length)
-    while(letters[counter] != words[rnd].charAt(0)){
+    while(letters[counter] != words[rnd].name[0]){
         rnd = Math.floor(Math.random()*words.length)
     }
     answers.push(words[rnd]);
     while(answers.length < 4){
         rnd = Math.floor(Math.random()*words.length)
-        if(letters[counter] != words[rnd].charAt(0) && !(answers.includes(words[rnd]))){
+        if(letters[counter] != words[rnd].name[0] && !(answers.includes(words[rnd].name))){
             answers.push(words[rnd]);
         }
     }
